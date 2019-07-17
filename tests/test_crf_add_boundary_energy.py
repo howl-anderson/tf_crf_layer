cf1cfa362eff46a2423ea39a0e5e1f1b7460a2c4
Xiaoquan Kong <u1mail2me@gmail.com>
2019-07-17 15:19:48 +0800

    Add test cases for boundary feature

---
+++ tests/test_crf_add_boundary_energy.py
@@ -0,0 +1,134 @@
+import numpy as np
+import tensorflow as tf
+
+from layer import CRF
+
+
+def test_crf_add_boundary_energy_with_no_mask():
+    sess = tf.InteractiveSession()
+    sess.as_default()
+
+    energy = tf.constant(
+        [
+            [
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0]
+            ],
+        ],
+        dtype=tf.float32
+    )
+
+    mask = None
+    start = tf.constant([1, 1, 1, 1, 1], dtype=tf.float32)
+    end = tf.constant([-1, -1, -1, -1, -1], dtype=tf.float32)
+
+    crf = CRF(None)
+    new_energy_tensor = crf.add_boundary_energy(energy, mask, start, end)
+
+    with tf.Session() as sess:
+        new_energy = sess.run(new_energy_tensor)
+
+    expected_energy = np.array(
+        [
+            [
+                [1, 1, 1, 1, 1],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [-1, - 1, - 1, - 1, - 1]
+            ],
+            [
+                [1, 1, 1, 1, 1],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [-1, - 1, - 1, - 1, - 1]
+            ]
+        ]
+    )
+
+    np.testing.assert_array_equal(new_energy, expected_energy)
+
+
+def test_crf_add_boundary_energy_with_mask():
+    sess = tf.InteractiveSession()
+    sess.as_default()
+
+    energy = tf.constant(
+        [
+            [
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0]
+            ],
+        ],
+        dtype=tf.float32
+    )
+
+    mask = tf.constant(
+        [
+            [0, 1, 1, 1, 1],  # pre padding
+            [1, 1, 1, 1, 0],  # post padding
+            [0, 1, 1, 1, 0]   # pre and post padding
+        ]
+    )
+    start = tf.constant([1, 1, 1, 1, 1], dtype=tf.float32)
+    end = tf.constant([-1, -1, -1, -1, -1], dtype=tf.float32)
+
+    crf = CRF(None)
+    new_energy_tensor = crf.add_boundary_energy(energy, mask, start, end)
+
+    with tf.Session() as sess:
+        new_energy = sess.run(new_energy_tensor)
+
+    expected_energy = np.array(
+        [[[0, 0, 0, 0, 0],
+          [1, 1, 1, 1, 1],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+          [-1, - 1, - 1, - 1, - 1]],
+
+         [[1, 1, 1, 1, 1],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+          [-1, - 1, - 1, - 1, - 1],
+          [0, 0, 0, 0, 0]],
+
+         [[0, 0, 0, 0, 0],
+          [1, 1, 1, 1, 1],
+          [0, 0, 0, 0, 0],
+          [-1, - 1, - 1, - 1, - 1],
+          [0, 0, 0, 0, 0]]]
+    )
+
+    np.testing.assert_array_equal(new_energy, expected_energy)
+
+
+if __name__ == "__main__":
+    test_crf_add_boundary_energy_with_mask()
