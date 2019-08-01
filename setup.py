e4e98692a5418f9b27ce225080d1eb2a29f923d1
Xiaoquan Kong <u1mail2me@gmail.com>
2019-08-01 14:46:03 +0800

    Update

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     packages=['tf_crf_layer'],
     url='URL',
     license='MIT',
+    install_requires=[
+          'tensorflow',
+    ],
+    tests_require=['pytest', 'numpy', 'keras'],
     author='Xiaoquan Kong',
     author_email='u1mail2me@gmail.com',
     description='CRF layer for TensorFlow 1.x'
--- tf_crf_layer/layer.py
+++ tf_crf_layer/layer.py
@@ -98,6 +98,9 @@ class CRF(Layer):
         self.left_boundary = None
         self.right_boundary = None
 
+        # variable used by unit test
+        self._debug_use_dense_layer = True
+
     def build(self, input_shape):
         input_shape = tuple(tf.TensorShape(input_shape).as_list())
         self.input_spec = [InputSpec(shape=input_shape)]
@@ -337,5 +340,8 @@ class CRF(Layer):
 
     def _dense_layer(self, input_):
         # TODO: can simply just use tf.keras.layers.dense ?
-        return self.activation(
-            K.dot(input_, self.kernel) + self.bias)
+        if self._debug_use_dense_layer:
+            return self.activation(
+                K.dot(input_, self.kernel) + self.bias)
+
+        return input_
