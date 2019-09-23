29e3f2ecdf7667c883b40d666f90044e76d85afa
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-23 13:47:59 +0800

    Update dependency

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ setup(
     url='URL',
     license='MIT',
     install_requires=[
-          'tensorflow',
+        'tensorflow',
+        "tokenizer_tools"
     ],
-    tests_require=['pytest', 'numpy', 'keras'],
+    tests_require=['pytest', 'numpy', 'keras', 'pandas'],
     author='Xiaoquan Kong',
     author_email='u1mail2me@gmail.com',
     description='CRF layer for TensorFlow 1.x'
